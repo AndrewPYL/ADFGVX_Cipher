ff9f0d9b3dd983ca7ea3a1a1202583b51d29beec
AndrewPYL <p2lai@ucsd.edu>
2024-03-14 21:20:21 -0700

    Add_all_pages
    1-21+map+teller

---
+++ src/components/P3.svelte
@@ -0,0 +1,150 @@
+<script>
+  export let index;
+</script>
+
+<div class="page-container">
+
+  {#if index <= 1}
+    <div class="background2" ></div>
+  {/if}
+  {#if index > 1}
+
+    <div class="background" ></div>
+    <div class="content">
+        <div class="title">
+            <p> The Origin of ADFGVX System </p>
+        </div>
+        <div class="founder">
+            <p> The ADFGVX Cipher system is Founded by Colonel Fritz Nebel – German radio officer, it had been used in WWI East Front since March, 1918. </p>
+        </div>
+        <div class="s2">
+            <p> Now, let's learn how to encrypt!</p>
+        </div>
+        <div class="image">
+          <img src="radio_4.jpg" alt="radio officer">
+        </div>
+    </div>
+  {/if}
+</div>
+
+<style>
+  @keyframes fadeIn {
+    from { opacity: 0; }
+    to { opacity: 1; }
+  }
+  .image {
+    opacity:0;
+    position:absolute;
+    top: 25vh;
+    left: 51%;
+    animation: fadeIn 0.5s ease-out forwards;
+    animation-delay: 2.5s;
+  }
+
+  @keyframes fadeOut {
+    from { opacity: 1; }
+    to { opacity: 0.1; }
+  }
+
+  .background {
+    background: black;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 1;
+    animation: fadeOut 0.3s ease-out forwards;
+    animation-delay: 0.5s;
+  }
+
+  .background2 {
+    background: black;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 2;
+  }
+
+  .page-container {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    margin: 0;
+  }
+
+
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    background: transparent;
+    width: 100%;
+    height: 100%;
+  }
+
+  @keyframes fadeInUp {
+    from {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+
+  .title {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    color: 	black;
+    text-align: center;
+    position: absolute;
+    left: 5%;
+    width: 90%;
+    font-weight: bold;
+    top: 3vh;
+    font-size: 50px;
+    opacity:0;
+    animation: fadeInUp 0.3s ease-out forwards;
+    animation-delay: 0.8s;
+  }
+
+  @keyframes fadeInLeft {
+    from {
+      opacity: 0;
+      transform: translateX(-20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+
+  .founder {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    color: 	black;
+    text-align: left;
+    position: absolute;
+    width: 600px;
+    font-weight: roman;
+    top: 25vh;
+    left: 11%;
+    font-size: 38px;
+    opacity:0;
+    animation: fadeInLeft 0.5s ease-out forwards;
+    animation-delay: 1.5s;
+  }
+
+  .s2 {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    color: 	black;
+    position: absolute;
+    font-weight: roman;
+    top: 75vh;
+    left: 30%;
+    font-size: 50px;
+    opacity:0;
+    animation: fadeInLeft 0.5s ease-out forwards;
+    animation-delay: 3.5s;
+  }
+
+  </style>
---
+++ src/components/ScrollyTeller.svelte
@@ -0,0 +1,255 @@
+<script>
+  import Scroller from "@sveltejs/svelte-scroller";
+  import Topic from "./Topic.svelte";
+  import P2 from "./P2.svelte";
+  import P3 from "./P3.svelte";
+  import P4 from "./P4.svelte";
+  import P5 from "./P5.svelte";
+  import P6 from "./P6.svelte";
+  import P8 from "./P8.svelte";
+  import P9 from "./P9.svelte";
+  import P10 from "./P10.svelte";
+  import P11 from "./P11.svelte";
+  import P12 from "./P12.svelte";
+  import P13 from "./P13.svelte";
+  import P14 from "./P14.svelte";
+  import P15 from "./P15.svelte";
+  import P16 from "./P16.svelte";
+  import P17 from "./P17.svelte";
+  import P18 from "./P18.svelte";
+  import P19 from "./P19.svelte";
+  import P20 from "./P20.svelte";
+  import P21 from "./P21.svelte";
+  import Map from "./Mapping.svelte";
+  import Point from './Svgmap2.svelte';
+
+  import InteractiveSection from "./InteractiveSection.svelte";
+
+  import { geoMercator } from "d3-geo";
+
+  let count, index, offset, progress;
+
+  let width, height;
+
+  let geoJsonToFit = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [1, 0],
+        },
+      },
+      {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [0, 1],
+        },
+      },
+    ],
+  };
+
+
+  
+</script>
+
+<Scroller
+  top={0.0}
+  bottom={1}
+  threshold={0.5}
+  bind:count
+  bind:index
+  bind:offset
+  bind:progress
+>
+  <div
+    class="background"
+    slot="background"
+    bind:clientWidth={width}
+    bind:clientHeight={height}
+  >
+    
+    <Map {index} />
+    
+    <div class="progress-bars">
+      <p>current section: <strong>{index + 1}/{count}</strong></p>
+      <progress value={count ? (index + 1) / count : 0} />
+
+      <p>offset in current section</p>
+      <progress value={offset || 0} />
+
+      <p>total progress</p>
+      <progress value={progress || 0} />
+    </div>
+
+
+    
+
+  </div>
+
+  <div class="foreground" slot="foreground">
+    
+    <section>
+      <div> 
+        <div><Topic/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P2 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P3 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P4 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P5 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P6 {index}/></div>
+      </div>
+    </section>
+    
+    <section> </section>
+
+    <section>
+      <div> 
+        <div><P8 {index}/></div>
+      </div>
+    </section>
+    
+    <section>
+      <div> 
+        <div><P9 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P10 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P11 {index}/></div>
+      </div>
+    </section>
+    
+    <section>
+      <div> 
+        <div><P12 {index}/></div>
+      </div>
+    </section>
+    
+    <section>
+      <div> 
+        <div><P13 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P14 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P15 {index}/></div>
+      </div>
+    </section>
+    
+    <section>
+      <div> 
+        <div><P16 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P17 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P18 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P19 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P20 {index}/></div>
+      </div>
+    </section>
+
+    <section>
+      <div> 
+        <div><P21 {index}/></div>
+      </div>
+    </section>
+
+    
+  </div>
+</Scroller>
+
+<style>
+  .background {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    outline: green solid 3px;
+  }
+
+  .foreground {
+    width: 100%;
+    margin: 0 auto;
+    height: auto;
+    position: relative;
+    outline: red solid 3px;
+  }
+
+  .progress-bars {
+    position: absolute;
+    background: rgba(170, 51, 120, 0.2) /*  40% opaque */;
+    visibility: visible;
+  }
+
+  section {
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.2); /* 20% opaque */
+    /* color: white; */
+    text-align: center;
+    color: black;
+    margin: 0 0 0 0;
+  }
+
+:global(body) {
+  margin: 0;
+}
+
+</style>
